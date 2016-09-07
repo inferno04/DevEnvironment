f9d9fcd1dffc2abdf7119b211ce2e3a012ad54bd
inferno04 <eginter04@gmail.com>
2016-09-07 00:07:34 -0400

    Changes to build.bat incorporated on non-networked Desktop
    Added some niceties to the default 'make.bat'.
    + Parameters for Debugger of choice
    + Parameters for cleaning utilities
    + Intermediate 'build' directory infrastructure so that .obj files don't
    go all over the damn place
    + Compiler Flags
    + Linking Libraries
    + More robust 'run' and 'debug' targets ( check for build dir )
    + Formatting ( I was worried about tabs, so everything is spaces )
    + Last revision date

--- make.bat
+++ make.bat
@@ -1,61 +1,135 @@
 :: make.bat
 :: Erik Ginter
+:: Last updated: September 7, 2016
 ::
 :: Emulate the build system I have on my Unix machines.
+::
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 @echo off
 
 setlocal
 
-set compiler=cl.exe
-set executable=program.exe
-set source=main.cpp
+  set delete_file=del /S /Q
+  set delete_dir=rmdir /S /Q
+
+:: Defaults set up for VS 2013+
+:: ( Assumes that you have already run vcvarsall.bat with desired arguments )
+  set compiler=cl.exe
+  set debugger=devenv.exe
+
+:: VS 2015 Useful Flag Table
+:: /Zi       Enable debugging information
+:: /ZI       Enable Edit-and-Continue debug info
+:: /Zo       Generate richer debugging information for optimized code
+:: /FC       Use full pathnames in diagnostics
+:: /GL       Enable link-time code generation
+:: /Gw       Separate Global variables for linker
+:: /GS       Enable security checks
+:: /analyze  Enable native analysis
+:: /MP[n]    Enable multi-process compilation
+  set compiler_flags=/Zi /FC /GL /Gw /GS /analyze /MP
+  set compiler_libs=
+  set executable=program.exe
+  set source=main.c
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 :: Argument / Parameter evaluation
-if "%~1"==""      ( goto build )
-if "%~1"=="clean" ( goto clean )
-if "%~1"=="run"   ( goto  run  )
+  if "%~1"==""      ( goto build_proc )
+  if "%~1"=="build" ( goto build_proc )
+  if "%~1"=="clean" ( goto clean_proc )
+  if "%~1"=="run"   ( goto  run_proc  )
+  if "%~1"=="debug" ( goto debug_proc )
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 :: Make ( Build )
-:build
+:build_proc
 
-%compiler% %source% /Fe%executable%
+  if not exist build mkdir build
 
-goto end
+  pushd build
+
+    @echo on
+
+      %compiler% %compiler_flags% ..\%source% /Fe%executable% %compiler_libs%
+
+    @echo off
+
+  popd build
+
+  goto eof
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 :: Make ( Clean )
-:clean
+:clean_proc
 
-:: Get rid of Emacs temp files
-del *~
-del #*#
+:: Get rid of compiler artifacts and the rest of the build directory
+  if exist build (
 
-:: Get rid of compiler artifacts
-del *.obj
+    @echo on
 
-:: Get rid of the executable
-del %executable%
+      %delete_dir% build
 
-goto end
+    @echo off
+
+  )
+
+:: Get rid of Emacs temp files ( If they exist at all )
+  @echo on
+
+    %delete_file% *~
+    %delete_file% #*#
+
+  @echo off
+
+  goto eof
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 :: Make ( Run )
 
-:run
+:run_proc
+
+:: Check to make sure our executable is built ( basic dependency )
+  if not exist build call make
 
 :: Run our compiled program
-%executable%
+  @echo on
 
-goto end
+    build\%executable%
+
+  @echo off
+
+  goto eof
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:: Make ( Debug )
+
+:debug_proc
+
+:: Check to make sure our executable is built ( basic dependency )
+  if not exist build call make
+
+:: Launch debugger with our executable
+:: ( Default set up for VS 2013+ DevEnv.exe - a GUI app )
+:: [ Remove "start" if using a CUI debugger ]
+
+  @echo on
+
+    start %debugger% build\%executable%
+
+  @echo off
+
+  goto eof
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 :: Make ( End )
-:end
+:eof
 
 endlocal
+
